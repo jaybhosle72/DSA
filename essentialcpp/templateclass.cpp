a55cdfaf267c08e25b67f5714553c7bc634aefc5
jaybhosle72 <jaybhosler@gmail.com>
2026-09-01 11:06:00 +0530

    used scope resolution operator

--- essentialcpp/templateclass.cpp
+++ essentialcpp/templateclass.cpp
@@ -9,24 +9,36 @@ class arithmetic{
 
     public:
 
-        arithmetic(t a ,t b){
-            this->a=a;
-            this->b=b;
-        }
-
-        t add(){
-            return a+b;
-        }
-
-        t sub(){
-            return 
-            a -b;
-        }    
+        arithmetic(t a,t b);
+        t add();
+        t sub();
 };
+
+template<class t>
+arithmetic<t>::arithmetic(t a,t b){
+    this->a=a;
+    this->b=b;
+}
+
+template<class t>
+t arithmetic<t>::add(){
+    return a+b;
+}
+
+template<class t>
+t arithmetic<t>::sub(){
+    return a-b;
+}
+
+
+        
+
+
 
 int main(){
     arithmetic<float> ar(5.6,6.4);
 
     cout<<ar.add()<<endl;
     cout<<ar.sub();
+    return 0;
 }
