f84161559d7ebe1e2d06f29d22ad970d40da5c9e
jaybhosle72 <jaybhosler@gmail.com>
2026-09-11 19:41:59 +0530

    better solution for power using recursion

--- recursion/powerrecursn.cpp
+++ recursion/powerrecursn.cpp
@@ -1,14 +1,35 @@
 #include<iostream>
 using namespace std;
+// classic solution:
+// int power(int m,int n){
+//     if(n==0){
+//         return 1;
+//     }
+//     return power(m,n-1)*m;
+// }
 
+// int main(){
+//     cout<<power(5,2);
+//     return 0;
+// }
+
+// better solution:
 int power(int m,int n){
-    if(n==0){
-        return 1;
+    if(n%2==0){
+        if(n==0){
+            return 1;
+        }
+        else{
+            return power(m*m,n/2);
+        }
     }
-    return power(m,n-1)*m;
-}
+    else{
+        return m*power(m*m,(n-1)/2);
+    }
+}    
+
 
 int main(){
-    cout<<power(5,2);
+    cout<<power(2,8);
     return 0;
 }
