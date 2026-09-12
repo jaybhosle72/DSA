c397b777a7607eb2147e44dda371bf247242c710
jaybhosle72 <jaybhosler@gmail.com>
2026-09-12 11:20:03 +0530

    update

--- recursion/powerrecursn.cpp
+++ recursion/powerrecursn.cpp
@@ -63,6 +63,27 @@ int main(){
     return 0;
 }
 
+// with iterative method
+
+// class solution{
+// public:
+//     int power(int m, int n){
+//         int ans=1;
+//         if(n==0){
+//             return 1;
+//         }
+//         for(int i=1;i<=n;i++){
+//             ans=ans*m;
+//         }
+//         return ans;
+
+//     }
+// };
+// int main(){
+//     solution s;
+//     cout<<s.power(2,2);
+//     return 0;
+// }
 
 
 
