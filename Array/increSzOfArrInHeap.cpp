22f8bcc33f5ca00ad491649dd163c6e39326152e
jaybhosle72 <jaybhosler@gmail.com>
2026-09-18 16:29:28 +0530

    update

--- Array/increSzOfArrInHeap.cpp
+++ Array/increSzOfArrInHeap.cpp
@@ -4,12 +4,22 @@ using namespace std;
 int main(){
     int *p=new int[5];
     int *q=new int[10];
+    p[0]=2;
+    p[1]=4;
+    p[2]=6;
+    p[3]=8;
+    p[4]=10;
 
     for(int i=0;i<5;i++){
         q[i]=p[i];
     }
     
+  
     delete []p;
     p=q;
     q=NULL;
+    for(int i=0;i<5;i++){
+        cout<<p[i];
+    }
+    
 }
