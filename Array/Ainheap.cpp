738e82c8d6a5280bdbd939b00f9510f809e7af59
jaybhosle72 <jaybhosler@gmail.com>
2026-09-18 12:12:37 +0530

    array in heap

---
+++ Array/Ainheap.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A[]={2,4,6,8,10};
+    int *p;
+    p=new int[5];
+    p[0]=1;
+    p[1]=3;
+    p[2]=5;
+    p[3]=7;
+    p[4]=9;
+
+    for(int i=0;i<5;i++){
+        cout<<A[i];
+    }
+    cout<<"\n";
+    for(int i=0;i<5;i++){
+        cout<<p[i];
+    }
+}
